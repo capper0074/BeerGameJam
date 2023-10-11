4c935e6dd67e050056c47b376ad344f2888b230e
dandydan112 <dan.t.nguyen123@gmail.com>
2023-10-11 14:04:20 +0200

    Jan level done (almost)

--- GameJam/Levels/PrinterLokale.cs
+++ GameJam/Levels/PrinterLokale.cs
@@ -15,11 +15,18 @@ namespace GameJam.Levels
         {
             Beautifier.CoolWrite("green", "Narrator: ", "white", "Du går ind i printerlokalet og støder ind i Jan" );
             Console.ReadKey();
+
             Beautifier.CoolWrite("blue", Player.name, "white", "Hvad er det du har i hånden Jan? :O");
             Console.ReadKey();
+            
             Beautifier.CoolWrite("red", "Jan: ", "white", "Jeg har en odense classic i hånden!!");
             Console.ReadKey();
-            //string playerChoice = Beautifier.CoolMenu("Hvad vil du nu", "Gå hjem", "Kig til højre"); //Implement menu with choices using beautifier methods
+            
+            Beautifier.CoolWrite("blue", Player.name, "white", "Hvad fanden laver du med min yndlingsdrink?!?!");
+            Console.ReadKey();
+            
+            Beautifier.CoolWrite("red", "Jan: ", "white", "Vil du have den, så kom og tag den");
+            string playerChoice = Beautifier.CoolMenu("Hvad har du tænkt dig at gøre?", "Gå hjem", "Flæk ham (combat)"); //"Flæk Ham" puts you into combat against Jan.
         }
     }
 }
