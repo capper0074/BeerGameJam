b05631b69050a59dac06f66717e11984e23e5272
ealbrechtsen <emilmalikalbrechtsen@gmail.com>
2023-10-11 13:08:57 +0200

    vendor igang

--- GameJam/Levels/Intro.cs
+++ GameJam/Levels/Intro.cs
@@ -14,22 +14,14 @@ namespace GameJam.Levels
     {
         public static void StartIntro()
         {
-            Beautifier.CoolWrite("green", $"{Player.name} er til fredagsbar i BeerBoxen. \n " +
-                $"Svælget bliver smurt med tonsvis af Odense Classic. Han taber lidt for mange \n " +
-                $"omgange beerpong. Øllen hober sig op og toilettet er den eneste udvej \n");
-            
-            string playerChoice = Beautifier.CoolMenu("What u want", "Valg1", "Show inventory");
+            AsciiArt.Ascii_Intro();
+            Beautifier.CoolWrite("green", $"Narrator: ");
+            Beautifier.CoolWrite("white", $"{Player.name} er til fredagsbar i BeerBoxen. \n " +
+                $"Svælget bliver smurt med tonsvis af Odense Classic. Han taber lidt for mange omgange beerpong.\n " +
+                $"Øllen hober sig op og toilettet er den eneste udvej. \n " +
+                $"Det sidste han ser, er sig selv i spejlet inden han falder omkuld med sin bajer i hånden.");
+             
 
-            if (playerChoice == "Valg1")
-            {
-                Controls.Control();
-            }
-            else if (playerChoice == "Show inventory")
-            {
-                Controls.Control();
-            } 
-
-            //
         }
     }
 }
---
+++ GameJam/Levels/Vendor.cs
@@ -0,0 +1,44 @@
+﻿using GameJam.Character;
+using GameJam.Graphic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameJam.Levels
+{
+    public static class Vendor
+    {
+        public static void VendorIntro(string vendor)
+        {
+            if (vendor.ToLower() == "person")
+            {
+                VendorPerson();
+            }
+            else if (vendor.ToLower() == "shop")
+            {
+                VendorShop();
+            }
+        }
+
+        public static void VendorShop()
+        {
+            Beautifier.CoolWrite("white", $"Narrator: ");
+            //Beautifier.CoolWrite("green", $"{Player.name} 
+        }
+
+        public static void VendorPerson()
+        {
+
+        }
+
+        //metode.. 
+        //vendor intro (kan både være butik eller person, to void metoder
+        //
+        //snakker med vendor
+        //ja nej
+        //hvis ja 
+        //hvis nej
+    }
+}
