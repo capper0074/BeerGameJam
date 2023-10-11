3aa734262b65049d8cd0915f68781cba36e5d643
Niko <Niko@DESKTOP-2R0EG79>
2023-10-11 10:08:19 +0200

    Inventory not implemented

--- GameJam/Character/Inventory.cs
+++ GameJam/Character/Inventory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GameJam.Stuff;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,12 @@ namespace GameJam.Character
 {
     internal class Inventory
     {
+        public List<Items> InventoryList { get; set; } //List containing the type Items
+
+
+        public void GetInventory()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
