bf9a1578c05b71853e5d0a860dfba91efb515a87
Casper <capper2704@hotmail.dk>
2023-10-12 12:58:20 +0200

    Combat should work

--- GameJam/Fighting/Combat.cs
+++ GameJam/Fighting/Combat.cs
@@ -1,4 +1,5 @@
 ﻿using GameJam.Character;
+using GameJam.GameStart;
 using GameJam.Graphic;
 using System;
 using System.Collections.Generic;
@@ -12,6 +13,7 @@ namespace GameJam.Fighting
     {
         public static bool StartCombat()
         {
+            Console.Clear();
             Beautifier.CoolWrite("red", "Welcome to combat, hope u are ready");
 
             Creatures enemy = Enemy.Enemy_Pick();
@@ -20,23 +22,48 @@ namespace GameJam.Fighting
 
             while(combatState == true)
             {
-                if (Player.Health < 0)
+                if (Player.Health > 0)
                 {
 
-                    if (enemy.Enemy_Health < 0)//Det skal være en modstander.
+                    if (enemy.Enemy_Health > 0)//Det skal være en modstander.
                     {
                         int combatRounds = 0;
                         Console.WriteLine("The combat starts");
-                        Console.WriteLine($"U use your {WeaponSlot.DisplayName} to attack {enemy.Name}");
-                        Console.WriteLine($"U deal {WeaponSlot.DisplayStat} to {enemy.Name}");
-                        Beautifier.CoolBarCombat(enemy.Enemy_Health,"Enemy health");
+
+                        if(Player.Piss >= 70)
+                        {
+                            string playerChoice = Beautifier.CoolMenu("Special attack", "Ja", "Nej");
+
+                            if(playerChoice == "Ja")
+                            {
+                                enemy.Enemy_Health -= 95;
+                                Beautifier.CoolBarCombat(enemy.Enemy_Health, "Enemy health");
+                            }
+                            else if(playerChoice == "Nej")
+                            {
+                                Console.WriteLine($"U use your {WeaponSlot.DisplayName()} to attack {enemy.Name}");
+                                Console.WriteLine($"U deal {WeaponSlot.DisplayStat()} to {enemy.Name}");
+                                enemy.Enemy_Health -= WeaponSlot.DisplayStat();
+                                Beautifier.CoolBarCombat(enemy.Enemy_Health, "Enemy health");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine($"U use your {WeaponSlot.DisplayName()} to attack {enemy.Name}");
+                            Console.WriteLine($"U deal {WeaponSlot.DisplayStat()} to {enemy.Name}");
+                            enemy.Enemy_Health -= WeaponSlot.DisplayStat();
+                            Beautifier.CoolBarCombat(enemy.Enemy_Health, "Enemy health");
+                        }
 
                         Console.WriteLine($"Now {enemy.Name} attacks u");
                         Console.WriteLine($"They deal a total of {enemy.Attack_Value} damage");
+                        Player.Health -= enemy.Attack_Value;
                         Beautifier.CoolBarCombat(Player.Health, "Player health");
+
 
                         Console.WriteLine("This was round: " + combatRounds);
                         Console.ReadKey();
+                        Console.Clear();
                         combatRounds++;
                     }
                     else
@@ -52,6 +79,9 @@ namespace GameJam.Fighting
                     Console.WriteLine("U lose combat noob");
                     return false; //u lost
                     combatState = false;
+                    Console.Clear();
+                    AsciiArt.Ascii_Lose();
+                    StartMenu.Menu();
                 }
 
                 
