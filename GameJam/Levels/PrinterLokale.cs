0292edf3ae8b07d851fe67cc6de1dddceffc9efb
dandydan112 <dan.t.nguyen123@gmail.com>
2023-10-13 08:52:53 +0200

    dan commit ændring

--- GameJam/Levels/PrinterLokale.cs
+++ GameJam/Levels/PrinterLokale.cs
@@ -70,6 +70,7 @@ namespace GameJam.Levels
                 Beautifier.CoolWrite("red", "Jan: ", "white", "NEEEEEEEJ DU BESEJREDE MIIIG");
                 Console.ReadKey();
                 Beautifier.CoolWrite("green", "Narrator: ", "white", "Uden sin øl, tog Jan hjem for lave flere af sine online tutorials");
+                Console.ReadKey();
                 #endregion
 
             }
